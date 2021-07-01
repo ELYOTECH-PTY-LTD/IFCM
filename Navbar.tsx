7bc85329dd07d93bdd16b18b51c2287df967246f
guillaume-elyotech <79210748+guillaume-elyotech@users.noreply.github.com>
2021-07-01 19:43:28 +0200

    add page

---
+++ Navbar.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useRef } from 'react';
+import { IonRouterOutlet, IonTabs, IonContent, IonHeader, IonPage, IonToolbar, IonButton, IonSlides,IonSlide,IonButtons,IonIcon,useIonViewWillEnter, IonImg, IonTitle, IonText, IonCard, IonCardContent, IonList, IonItem, IonAvatar, IonFooter, IonLabel, IonTabButton,IonTabBar } from '@ionic/react';
+import { arrowForward, bookmarksSharp, homeOutline, bagOutline, bookOutline, heart, person, personOutline, heartOutline } from 'ionicons/icons';
+import './Navbar.scss';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { Route, Redirect } from 'react-router';
+import { IonReactRouter } from '@ionic/react-router';
+import { Link } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import history from '../history';
+import UserService from "../services/UserService";
+import { BookmarkSharp,  HomeOutline , BagOutline, HeartOutline , PersonOutline} from 'react-ionicons';
+
+
+import Homepage from './Homepage';
+import EventDetails from './EventDetails';
+import Teaching from './Teaching';
+import TeachingList from './TeachingList';
+import TeachingDetails from './TeachingDetails';
+import TeachingOrder from './TeachingOrder';
+import Signup from './Signup';
+import Login from './Login';
+import Welcome from './Welcome';
+import Shopping from './Shopping';
+import UserProfile from './UserProfile';
+import Donation from './Donation';
+import AboutUs from './AboutUs';
+import PaymentCardDetail from './PaymentCardDetail';
+import PaymentShippingDetail from './PaymentShippingDetail';
+
+import Shoppingitemdetails from './Shoppingitemdetails';
+import ShoppingCart from './ShoppingCart'
+
+import PaymentDeliveryDetail from './PaymentDeliveryDetail';
+import OrderList from './OrderList';
+import OrderDetail from './OrderDetail';
+
+interface MainTabsProps { }
+
+const Navbar: React.FC<MainTabsProps> = () => {
+
+
+  return (
+    <IonTabs>
+      <IonRouterOutlet>
+        <Redirect exact path="tabs" to="tabs/home" />
+        {/*
+          Using the render method prop cuts down the number of renders your components will have due to route changes.
+          Use the component prop when your component depends on the RouterComponentProps passed in automatically.
+        */}
+        <Route path="/tabs/home" render={() => <Homepage />} exact={true} />
+        <Route path="/tabs/home/eventdetails/:id" component={EventDetails} />
+        <Route path="/tabs/shopping" render={() => <Shopping />} exact={true} />
+        <Route path="/tabs/teaching" render={() => <Teaching />} exact={true} />
+        <Route path="/tabs/teaching/teachinglist" component={TeachingList} exact={true} />
+        <Route path="/tabs/teaching/teachingdetails" component={TeachingDetails} />
+        <Route path="/tabs/teaching/teachingorder" component={TeachingOrder} />
+        <Route path="/tabs/shopping" render={() => <Shopping />} exact={true} />
+        <Route path="/tabs/donation" render={() => <Donation />} exact={true} />
+        <Route path="/tabs/userprofile" render={() => <UserProfile />} exact={true} />
+        <Route path="/tabs/userprofile/aboutus" component={AboutUs} />
+        <Route path="/tabs/userprofile/orderlist" component={OrderList} />
+        <Route path="/tabs/userprofile/orderdetail" component={OrderDetail} />
+        <Route path="/tabs/shopping/Shoppingitemdetails" component={Shoppingitemdetails} />
+        <Route path="/tabs/shopping/Shoppingitemdetails/ShoppingCart" component={ ShoppingCart} />
+       
+      </IonRouterOutlet>
+
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="home" href="/tabs/home">
+          <IonIcon icon={homeOutline} />
+          <IonLabel>Home</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="shopping" href="/tabs/shopping">
+          <IonIcon icon={bagOutline} />
+          <IonLabel>Shop</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="teaching" href="/tabs/teaching">
+          <IonIcon icon={bookOutline} />
+          <IonLabel>Teachings</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="donation" href="/tabs/donation">
+          <IonIcon icon={heartOutline} />
+          <IonLabel>Donation</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="userprofile" href="/tabs/userprofile">
+          <IonIcon icon={personOutline} />
+          <IonLabel>Profile</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+
+
+        
+  );
+
+};
+
+export default withRouter (Navbar);
---
+++ OrderList.tsx
@@ -0,0 +1,83 @@
+
+import React, { useState, useRef } from 'react';
+import { IonListHeader,IonThumbnail,IonContent, IonHeader, IonPage, IonToolbar, IonButton, IonSlides,IonSlide,IonButtons,IonIcon,useIonViewWillEnter, IonImg, IonTitle, IonText, IonCard, IonCardContent, IonCardSubtitle ,IonList, IonItem, IonAvatar, IonFooter, IonLabel, IonTabButton,IonTabBar, IonGrid, IonRow, IonCol, IonCardTitle, IonCardHeader,IonBackButton} from '@ionic/react';
+import { arrowBack, arrowForward, bookmarksSharp , lockClosedOutline} from 'ionicons/icons';
+import './OrderList.scss';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { IonReactRouter } from '@ionic/react-router';
+import { Link } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+import history from '../history';
+import UserService from "../services/UserService";
+import { BookmarkSharp,  HomeOutline , BagOutline, HeartOutline , PersonOutline ,ArrowBackOutline , LockClosedOutline , ArrowForwardOutline} from 'react-ionicons'
+type EventItem = {
+    id: string;
+    src: string;
+    nombre: string;
+    categorie: string;
+
+  };
+  const items: EventItem[] = [{ id: '1', src: '/assets/img/israel_pilgrimage.jpg', nombre: '2', categorie:'Music'},
+  {id: '1', src: '/assets/img/israel_pilgrimage.jpg', nombre: '2', categorie:'Music'},
+  {id: '1', src: '/assets/img/israel_pilgrimage.jpg', nombre: '2', categorie:'Music'},
+  { id: '1', src: '/assets/img/israel_pilgrimage.jpg', nombre: '2', categorie:'Music'}];
+  
+
+type SomeComponentProps = RouteComponentProps;
+const OrderList: React.FC<SomeComponentProps> = ({ history }) => {
+
+    return (
+        <IonPage id="teachingorder-page">
+          <IonContent fullscreen>
+            
+                <IonButtons>
+                    <IonBackButton defaultHref="/tabs/userprofile" className="back"/>
+                </IonButtons>
+                <IonText className="titre_order"> My Order </IonText>
+              <br/>
+              <br/>
+              <br/>
+              <hr className="trait_simple"></hr>
+              <IonList className="event" lines="none">
+            <IonListHeader className="titre_orders">Last Added </IonListHeader>
+            <br/>
+            <br/>
+
+
+              {items.map((image, i) => (
+        <IonItem className="event_card" key={i} routerLink={`/tabs/userprofile/orderdetail/${image.id}`}>
+            <IonThumbnail slot="start" className="order_img">
+            <img src={image.src} className="event-img"/>
+            </IonThumbnail>
+            <IonLabel>
+              <br/>
+            <h3 className="date_event"><b> {image.categorie} </b></h3>
+            <h2 className="lieu_event"> {image.nombre} </h2>
+            <ArrowForwardOutline
+              color={'#00000'} 
+              cssClasses="button_order"
+              height="25px"
+              width="25px"
+            />
+            <hr className="trait_simple"></hr>
+            
+            </IonLabel>
+
+
+            </IonItem>
+      ))}
+
+ </IonList>
+
+              
+
+         
+
+          </IonContent>
+                
+        </IonPage>
+        
+        
+  );
+}
+export default withRouter (OrderList);
